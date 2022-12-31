1c847e146b4fd3b5b34f7dbe0c9f7a3756feda40
stickz <stickman002@mail.com>
2022-12-31 03:21:32 -0500

    Fix syntax error

--- functions.sh
+++ functions.sh
@@ -2767,7 +2767,7 @@ extract_image() {
         bsdCOMPRESSION="-j"
        ;;
       txz)
-        tarCOMPRESSION="-I 'xz -T4' "
+        tarCOMPRESSION="-I 'xz -T 0' "
         bsdCOMPRESSION="-J"
        ;;
       *)return 1;;
