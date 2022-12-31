dc90f247e71ca1784614bbeceecea8a7d371a249
stickz <stickman002@mail.com>
2022-12-31 00:16:14 -0500

    clarify xz threads

--- functions.sh
+++ functions.sh
@@ -2768,7 +2768,7 @@ extract_image() {
        ;;
       txz)
         tarCOMPRESSION="-I \"xz -T0\" "
-        bsdCOMPRESSION="-J --options='threads=0' "
+        bsdCOMPRESSION="-J --options='xz:threads=0' "
        ;;
       *)return 1;;
     esac
